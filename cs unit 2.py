943444ad7ef0e55a2d139116f795ee14eea92c8e
elvag22 <elva.gao@thekaustschool.org>
2024-11-26 08:59:00 +0300

    Create cs unit 2.py

---
+++ cs unit 2.py
@@ -0,0 +1,15 @@
+#Base choice
+while True:
+    print("\nDo you want to take the quiz first (1), or read the information first (2)?")
+    choice = input("Enter your choice: ").lower()
+    if choice == "1":
+        learninformation = ("Sorting trash reduces waste in landfills, saves resources, and prevents pollution. It helps recycle materials and ensures hazardous items don’t harm the environment."
+"\nHow to Organize Trash:"
+"\n-Recycling Bin: Clean paper, cardboard, glass, metal, and certain plastics."
+"\n-Compost Bin: Food scraps, coffee grounds, and yard waste."
+"\n-Trash Bin: Non-recyclable items like dirty plastics or non-compostable food packaging."
+"\n-Hazardous Waste: Batteries, electronics, and chemicals")
+
+        print(learninformation)
+
+    break
